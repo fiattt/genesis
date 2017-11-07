7ab8f17df158d2faf670e29772acf416cedb6a59
Sandwich <hi.na3s@gmail.com>
2017-11-07 15:07:20 +0100

    simplify tests

--- tools/snapshot/tasks/misc/tests.js
+++ tools/snapshot/tasks/misc/tests.js
@@ -1,15 +1,22 @@
-let table = require('ascii-table')
+module.exports = ( state, complete ) => {
+  const Table = require( 'ascii-table' ),
+        colors = require( 'colors/safe' ),
+        async = require( 'async' )
 
-module.exports = ( complete ) {
-  async.series([
+  async.waterfall([
+    next => next(null, state),
     require('../../tests/total-daily-buys.js'),
     require('../../tests/negative-balances.js')
-  ], (error) => {
-    if(error)
-      complete()
-    else
-      throw new Error(error)
-      table.row()
+  ], (error, results) => {
+    if(!error)
+      throw new Error(error),
       process.exit()
+    else {
+      let table = new Table('Tests')
+      table.addRow('Daily Buys', '✓')
+      table.addRow('Negative Balances', '✓')
+      console.log(colors.green(table.render()))
+      setTimeout( () => complete( null, state ), 5000 )
+    }
   })
 }
