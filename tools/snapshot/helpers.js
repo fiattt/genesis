e88e43e5f3f6618947bb28def52201e7fb534ce1
Sandwich <hi.na3s@gmail.com>
2017-11-07 18:33:34 +0100

    remove deprecated/redundant helpers

--- tools/snapshot/helpers.js
+++ tools/snapshot/helpers.js
@@ -1,10 +1,10 @@
-const async        = require('async')
-const Sequelize    = require('Sequelize')
-const Op           = Sequelize.Op
-const bn           = require('bignumber.js')
+const async        = require('async'),
+      Sequelize    = require('Sequelize'),
+      Op           = Sequelize.Op,
+      bn           = require('bignumber.js')
 
-let query          = require('./queries'),
-    address        = new Object()
+let   query        = require('./queries'),
+      address      = {}
 
 address.uniques = (begin, end, uniques_found) => {
 
@@ -42,78 +42,6 @@ address.uniques = (begin, end, uniques_found) => {
     ],
     () => { uniques_found( uniques ) }
   )
-}
-
-address.last_register = query.last_register
-
-// address.sum_wallet_balance = ( address, begion, end, sum ) => {
-//   db.sequelize
-//     .query('SELECT sum()', {type: db.sequelize.QueryTypes.SELECT})
-// }
-
-address.transfers = (address, begin, end, transfers_found) => {
-  let transfers = []
-
-  const add = next => {
-    query.address_transfers_in(address, begin, end)
-      .then( results => {
-        let _results = results.map( result => new bn(result.dataValues.eos_amount) )
-        transfers = transfers.concat(_results)
-        next()
-      })
-  }
-
-  const subtract = next => {
-    query.address_transfers_out(address, begin, end)
-      .then( results => {
-        let _results = results.map( result => new bn(result.dataValues.eos_amount).times(-1) )
-        transfers = transfers.concat(_results)
-        next()
-      })
-  }
-
-  async.series([
-    add,
-    subtract
-  ], () => { transfers_found(transfers) })
-}
-
-address.buys = (address, begin, end, buys_found) => {
-  query.address_buys(address, begin, end)
-    .then( results => {
-      let buys = new Array( CS_NUMBER_OF_PERIODS ).fill( new bn(0) )
-      results.forEach( result => {
-        buys[ result.dataValues.period ] = buys[ result.dataValues.period ].plus( new bn(result.dataValues.eth_amount) )
-      })
-      buys_found(buys)
-    })
-}
-
-address.reclaimables = ( address, begin, end, reclaimables_found ) => {
-  let reclaimables = []
-  query.address_reclaimables( address, begin, end )
-    .then( results => {
-      if( results.length ) {
-        reclaimables = results.map( result => { return { address: address, value: result.dataValues.eos_amount } } )
-        console.log(reclaimables)
-      }
-      reclaimables_found( null, reclaimables )
-    })
-}
-
-address.claims = (address, begin, end, claims_found) => {
-  query.address_claims(address, begin, end)
-    .then( results => {
-      let claims = new Array( CS_NUMBER_OF_PERIODS ).fill( false )
-      results.forEach( result => {
-        claims[ result.dataValues.period ] = true
-      })
-      claims_found(claims)
-    })
-}
-
-address.bulk_upsert = ( addresses ) => {
-  return query.wallets_bulk_upsert( addresses )
 }
 
 module.exports = { address : address }
