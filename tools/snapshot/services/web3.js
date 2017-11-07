ad91f060dd55743f330db25d1f6885d5b6c0b23d
Sandwich <hi.na3s@gmail.com>
2017-11-07 15:10:51 +0100

    moved web3 helpers into service

--- tools/snapshot/services/web3.js
+++ tools/snapshot/services/web3.js
@@ -5,6 +5,8 @@ let web3,
     type = typeof SS_CONFIG_ETHAPI_TYPE!=="undefined" ? SS_CONFIG_ETHAPI_TYPE : null,
     path = typeof SS_CONFIG_ETHAPI_PATH!=="undefined" ? SS_CONFIG_ETHAPI_PATH : null
 
+console.log(type, path)
+
 if(type == 'ipc' && typeof path !== null)
   web3  = new Web3( new Web3.providers.IpcProvider( path, require('net') ) )
 else if(type == 'ws')
