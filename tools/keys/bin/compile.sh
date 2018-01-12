fe210331e4f108506b6cb43b99da971fa35b6700
Sandwich <hi.na3s@gmail.com>
2018-01-12 21:01:23 +0100

    compile for local system

--- tools/keys/bin/compile.sh
+++ tools/keys/bin/compile.sh
@@ -5,4 +5,4 @@ version="$(node -pe "JSON.parse(\`$str\`)['version']")"
 file="EOS-Genesis-Keys-Utility-${version}"
 rm -fr ./build
 mkdir ./build
-electron-packager . $file --platform=all --out=build
+electron-packager . $file --out=build
