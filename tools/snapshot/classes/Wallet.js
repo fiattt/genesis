c98f2371b59a373f15504e42c63528fba63a4e34
Sandwich <hi.na3s@gmail.com>
2018-01-19 01:15:20 +0100

    commented

--- tools/snapshot/classes/Wallet.js
+++ tools/snapshot/classes/Wallet.js
@@ -103,7 +103,7 @@ class Wallet {
   validate(){
     let error = null
 
-    //Because we haven't toFormat(4) yet, there are some balances that may be wrongly attributed this error. We'll test against decimal precision 4.
+    //Because we haven't toFormat(4) yet, there will be some balances that may be wrongly attributed this error. We'll test against decimal precision 4.
     //Consider toFixed() before validation!
     if( parseFloat(this.balance.total.toFixed(4)) < parseFloat(1.0000) ) {
       error = 'balance_insufficient'
