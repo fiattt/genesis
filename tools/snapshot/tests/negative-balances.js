c3a597b227e4c83f0d175093a97e72a4ffd5e76f
Sandwich <hi.na3s@gmail.com>
2018-01-19 01:11:12 +0100

    fixed test

--- tools/snapshot/tests/negative-balances.js
+++ tools/snapshot/tests/negative-balances.js
@@ -5,11 +5,10 @@ module.exports = ( state, callback ) => {
   db.sequelize
     .query(`SELECT address FROM wallets WHERE balance_wallet<0 OR balance_unclaimed<0 OR balance_reclaimed<0`, {type: db.sequelize.QueryTypes.SELECT})
     .then(anomalies => {
-      if(anomalies)
+      if(anomalies.length)
         state.tests.negative_balances = `Found ${anomalies.length} Wallets with Negative Balances!!! [SELECT address FROM wallets WHERE balance_wallet<0 OR balance_unclaimed<0 OR balance_reclaimed<0]`
       else
         state.tests.negative_balances = true
-
       callback(null, state)
     })
 }
