a60d2ff22ae3498da1a76c8bf65644a8739c1385
Sandwich <hi.na3s@gmail.com>
2018-03-29 17:20:36 +0200

    fix block range logic

--- tools/snapshot/tasks/sync/block-range.js
+++ tools/snapshot/tasks/sync/block-range.js
@@ -1,7 +1,7 @@
 module.exports = ( state, complete )  => {
-  if(state.frozen > 0 && config.mode == 'mainnet') {
+  if(state.frozen > 0 && state.freeze_block) {
     state.block_begin = CS_BLOCK_FIRST
-    state.block_end = state.frozen
+    state.block_end = state.freeze_block
   } else {
     state.block_begin         = state.period_map[0].begin
     state.block_end           = state.period_map[config.period].end
