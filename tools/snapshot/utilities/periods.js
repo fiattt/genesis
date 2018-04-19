898883bda105825b71392dd2527d3f1a59ddb8cb
Sandwich <hi.na3s@gmail.com>
2018-04-20 00:56:52 +0200

    added catch

--- tools/snapshot/utilities/periods.js
+++ tools/snapshot/utilities/periods.js
@@ -34,10 +34,18 @@ const end_to_block = ( period_map, period ) => {
 
 const daily_totals = ( callback ) => {
   let contract  = require('../helpers/web3-contract')
-  contract.$utility.methods.dailyTotals().call().then( totals => {
-    let t = totals.map( total => new bn(total) );
-    callback(t)
-  })
+  contract.$utility.methods.dailyTotals().call()
+    .then( totals => {
+      let t = totals.map( total => new bn(total) );
+      callback(t)
+    })
+    .catch( e => { throw new Error(e)} )
+}
+
+const expected_supply = (period, include_b1) => {
+  let result = new bn(CS_CREATE_FIRST_PERIOD).div(WAD).plus( new bn(CS_CREATE_PER_PERIOD).div(WAD).times(new bn(period)) )
+  if(include_b1) result.plus(new bn(CS_B1_DISTRIBUTION).div(WAD))
+  return result.toFixed(4)
 }
 
 module.exports = {
@@ -46,5 +54,6 @@ module.exports = {
   last_closed: last_closed,
   begin_to_block: begin_to_block,
   end_to_block: end_to_block,
-  daily_totals: daily_totals
+  daily_totals: daily_totals,
+  expected_supply: expected_supply
 }
