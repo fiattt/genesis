a066a90eaf2098d491192bb7d17d2540b1a3bb77
Sandwich <hi.na3s@gmail.com>
2018-04-24 10:03:40 +0200

    Update config.default.js

--- config.default.js
+++ config.default.js
@@ -2,16 +2,8 @@ module.exports = {
   //Take snapshot up to which period? If left undefined, it will default to the last closed period.
   period: 0,
 
-  //Run in which mode.
-  // test - will generate a snapshot up to period defined.
-  // mainnet - will ignore period parameter, check if the dist. is over and if the tokens are frozen, and only then generate a snapshot.
-  mode: 'test',
-
   //The Author
   author: "Anonymous",
-
-  //Enable fallback 1.0? (not recommended)
-  registration_fallback: false,
 
   //Include block.one distribution in snapshot
   include_b1: true,
@@ -19,6 +11,9 @@ module.exports = {
   //Minimum balance required for snapshot inclusion.
   //Note: 1 EOS is recommended, as there will be a minimum balance required to have the bandwidth required for a functional account. Additionally, this prevents dust from appearing as an initial accounts, and cleans up the chain.
   snapshot_minimum_balance: 1,
+  
+  //Overwrite the snapshot.json in root directory.
+  overwrite_snapshot: true,
 
   //ETH node Connection Details
   eth_node_type: '',
@@ -40,15 +35,15 @@ module.exports = {
   // Probably do not need to change this.
   //
 
-  //Cache Signatures from/for fallback (speeds up multiple runs)
-  cache_signatures: true,
-
-  //Overwrite the snapshot.json in root directory.
-  overwrite_snapshot: true,
-
   //Recalculate wallets table based on already synced
   recalculate_wallets: false,
 
   //For if parity is syncing but you're testing for a period that you know is already synced.
-  skip_web3_sync: false
+  skip_web3_sync: false,
+
+  //Enable fallback 1.0? (deprecated) 
+  //registration_fallback: false,
+  
+  //Cache Signatures from/for fallback (deprecated)
+  //cache_signatures: true,
 }
