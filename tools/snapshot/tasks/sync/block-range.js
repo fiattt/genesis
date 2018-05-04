8f16a5861bba01d756d45f24434301324b84fc73
Sandwich <hi.na3s@gmail.com>
2018-05-05 00:25:45 +0200

    mode now set by block range

--- tools/snapshot/tasks/sync/block-range.js
+++ tools/snapshot/tasks/sync/block-range.js
@@ -2,12 +2,12 @@ module.exports = ( state, complete )  => {
   if(state.frozen > 0 && state.freeze_block) {
     state.block_begin         = CS_BLOCK_FIRST
     state.block_end           = state.freeze_block
-    global.config.mode        = "final"
+    state.mode                = "final"
     config.period             = CS_MAX_PERIOD_INDEX
   } else {
     state.block_begin         = state.period_map[0].begin
     state.block_end           = state.period_map[config.period].end
-    global.config.mode        = "ongoing"
+    state.mode                = "ongoing"
   }
   complete(null, state)
 }
