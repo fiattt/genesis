6459cc2ffbb7146c4630a635ea69ca995722a3cc
Sandwich <hi.na3s@gmail.com>
2018-05-07 20:38:58 +0200

    fail - testing more deterministic public key upsert with conditional update

--- tools/snapshot/tasks/sync/public_keys_child_process.js
+++ tools/snapshot/tasks/sync/public_keys_child_process.js
@@ -1,5 +1,7 @@
 require('../../lib/globals')
 
+const CACHE_THRESHOLD = 10
+
 let   id,
       state,
       thread_block_begin,
@@ -33,6 +35,7 @@ const setup = settings => {
 
 const run = () => {
   const db             = require('../../models'),
+        query          = require('../../queries'),
         db_config      = {ignoreDuplicates: true},
         colors         = require('colors/safe'),
         _for           = require('async-for'),
@@ -46,6 +49,7 @@ const run = () => {
         log_intval,
         iterator,
         cache = [],
+        cache_length = 0,
         block_current = 0,
         log = ""
 
@@ -67,9 +71,18 @@ const run = () => {
     )
   }
 
+  const build_public_key_query = (record) => {
+    //Multiple threads can create indeterminism in this table, particularly if public key is added during period cross-over.
+    //Conditional on duplicate key update will maintain determinism in this table, always save the lowest block number.
+    // const query = `INSERT INTO public_keys (address, public_key, block_number) VALUES ('${record.address}', '${record.public_key}', ${record.block_number}) ON DUPLICATE KEY UPDATE block_number = IF(block_number > ${record.block_number}, ${record.block_number}, block_number); `
+    const query = `INSERT INTO public_keys (address, public_key, block_number) VALUES ("${record.address}", "${record.public_key}", ${record.block_number}) ON DUPLICATE KEY UPDATE block_number = CASE WHEN VALUES(block_number) < block_number THEN VALUES(block_number) ELSE block_number END;`
+    return query
+  }
+
   const find_sync_pub_key = (tx, finished) => {
     pubkey_from_tx_hash(tx.hash, pubkey => {
       cache.push({ address: tx.from, public_key: pubkey, block_number: tx.blockNumber })
+      // cache_length++
       finished()
     })
   }
@@ -87,7 +100,7 @@ const run = () => {
           .then( block => {
             if(current_block == thread_block_end)
               console.log(`Thread ${id}: Last Pass Detected - Current:${current_block}, End: ${thread_block_end}`)
-            if(cache.length>10000 || current_block == thread_block_end)
+            if(cache.length>CACHE_THRESHOLD || current_block == thread_block_end)
               save_rows( () => { process_transactions(block, _continue ) } )
             else
               process_transactions(block, _continue )
@@ -108,6 +121,22 @@ const run = () => {
    }
 
   const save_rows = ( callback, deadlock ) => {
+    // console.log(cache)
+    // query.public_keys_bulk_upsert( cache )
+    //   .then( result => {
+    //     console.log(result)
+    //     if(deadlock) console.log(`${id} DEADLOCK: RESOLVED`)
+    //     cache = ""
+    //     cache_length = 0
+    //     update_state()
+    //     callback()
+    //   })
+    //   .catch( e => {
+    //     //We assume this is a deadlock, if you get repeated unresolved deadlocks, uncomment line below.
+    //     throw new Error(e)
+    //     console.log(`Thread ${id}: DEADLOCK: RETRY`)
+    //     setTimeout( () => save_rows(callback, true), 500 )
+    //   })
     db.Keys.bulkCreate( cache, {ignoreDuplicates: true} )
       .then( () => {
         if(deadlock) console.log(`${id} DEADLOCK: RESOLVED`)
