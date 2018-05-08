ece6dd88537fa416f3b3dee2f89d6fe14dcfcee7
Sandwich <hi.na3s@gmail.com>
2018-05-08 12:00:34 +0200

    optimized period block range discovery

--- tools/snapshot/classes/PeriodMap.js
+++ tools/snapshot/classes/PeriodMap.js
@@ -26,18 +26,21 @@ class PeriodMap {
   generate( onComplete = () => {} ){
 
     let map             = this.map,
-        period_max      = this.periodMax ? this.periodMax : util.period.last_closed(),
+        period_max      = typeof this.periodMax === 'number' ? this.periodMax : util.period.last_closed(),
         period_today    = util.period.from_date( Date.now()/1000 | 0 ),
-        block_index     = map.length ? map[map.length-1].end+1 : CS_BLOCK_FIRST,
+        walk_index      = map.length ? map[map.length-1].end+1 : CS_BLOCK_FIRST,
         block_cache     = null,
         period_index    = null,
-        period_cache    = null
+        period_cache    = null,
+
+        sprint          = true,
+        sprint_index    = walk_index,
+        sprint_steps    = 100
 
     if(typeof this.map[CS_MAX_PERIOD_INDEX] !== 'undefined' && typeof this.map[CS_MAX_PERIOD_INDEX].end === "number") {
       onComplete(this)
       return
     }
-
 
     if(period_max > CS_MAX_PERIOD_INDEX) {
       period_max = CS_MAX_PERIOD_INDEX
@@ -52,43 +55,69 @@ class PeriodMap {
     }
 
     const iterate = () => {
-      util.block.get( block_index, block => {
-          //Derive a period number from the block's timestamp
-          period_index = util.period.from_date( block.timestamp )
+      let block_index = sprint ? sprint_index : walk_index
+      util.block.get( block_index , block => {
+        // if(sprint) {
+        //   console.log(colors.magenta(`Sprinting: ${sprint_index}`))
+        // } else {
+        //   console.log(colors.yellow(`Walking: ${walk_index}`))
+        // }
 
-          //First run won't have a cache, set cache to previously defined index.
-          if(period_cache === null) period_cache = period_index
+        //Derive a period number from the block's timestamp
+        period_index = util.period.from_date( block.timestamp )
 
-          //The period has changed save the previously cached block number to the previous period "end"
+        //First run won't have a cache, set cache to previously defined index.
+        if(period_cache === null) period_cache = period_index
+
+        //IF the period index does not yet exist in the map
+        //..AND the index is less than the "highest" period
+        //..AND the map length is less than the total number of periods (351)
+        //..THEN add the block range object to the array and set the "begin" blocknumber of that period since we just discovered it.
+        if(sprint){
           if(period_index != period_cache)
-            map[period_cache].end = block_cache,
-            console.log(colors.green(`Added period ${period_cache} to the Period Map :)`)),
-            console.log(map[map.length-1])
-
-          //IF the period index does not yet exist in the map
-          //..AND the index is less than the "highest" period
-          //..AND the map length is less than the total number of periods (351)
-          //..THEN add the block range object to the array and set the "begin" blocknumber of that period since we just discovered it.
-          if(typeof map[period_index] !== "object" && period_index <= period_max && map.length < CS_NUMBER_OF_PERIODS)
-            map.push({ begin: block_index, end: null })
-
-          //Save this period index to detect period change on next run.
-          period_cache = parseInt(period_index)
-
-          //If a period change is detected, we'll need this on next run to set the previous period's "end block"
-          block_cache  = parseInt(block_index)
-
-          //Check block on next run.
-          block_index++
-
-          //All period block ranges have been discovered
-          if(map.length == CS_NUMBER_OF_PERIODS && map[CS_MAX_PERIOD_INDEX].end != null)
-            this.map = map,
-            onComplete( this )
-          //Still have more ranges to discover
+            walk_index = sprint_index-sprint_steps,
+            // console.log(`SPRINT: detected new period in ${sprint_index}, setting block index to ${walk_index} and walking.`),
+            sprint = false
           else
-            ( period_cache <= period_max ) ? setTimeout(iterate, 0) : (this.map = map, onComplete( this ))
-        })
+            sprint_index += sprint_steps
+          iterate()
+          return
+        }
+
+        //The period has changed, save the previously cached block number to the previous period "end"
+        if(period_index != period_cache)
+          map[period_cache].end = block_cache,
+          console.log(colors.green(`Added period ${period_cache} to the Period Map :)`)),
+          console.log(map[map.length-1]),
+          // console.log(`WALK: found deterministic end block for ${period_cache} at ${block_index}, going to sprint again`),
+          sprint_index = block_index,
+          sprint = true
+
+        if(typeof map[period_index] !== "object" && period_index <= period_max && map.length < CS_NUMBER_OF_PERIODS) {
+          if(period_index == 0) {
+            map.push({ begin: CS_BLOCK_FIRST, end: null })
+          } else {
+            map.push({ begin: map[period_index-1].end+1, end: null })
+          }
+        }
+
+        //Save this period index to detect period change on next run.
+        period_cache = parseInt(period_index)
+
+        //If a period change is detected, we'll need this on next run to set the previous period's "end block"
+        block_cache  = parseInt(block_index)
+
+        //Check block on next run.
+        walk_index++
+
+        // if(map.length == CS_NUMBER_OF_PERIODS && map[CS_MAX_PERIOD_INDEX].end != null)
+        //Still have more ranges to discover
+        if( period_cache <= period_max )
+          iterate()
+        else
+          this.map = map,
+          onComplete( this )
+      })
     }
 
     message()
