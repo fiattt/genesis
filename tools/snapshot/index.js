1245e4541824062986dcafe26ad28a3efe4d074c
Sandwich <hi.na3s@gmail.com>
2018-05-18 17:53:55 +0200

    clean up the index

--- tools/snapshot/index.js
+++ tools/snapshot/index.js
@@ -11,60 +11,11 @@ module.exports = (COMPLETE) => {
 
   let IS_SETUP = false
 
-  const show_prompt = next => {
-    prompt = require('./prompt')
-    prompt.start()
-    prompt.get( prompt.schema, (error, config) => {
-      if(error)
-        throw new Error(error)
-      else
-        next(null, config)
-    })
-  }
-
-  const load_validate_config = (config, next) => {
-    if(config.load_config) {
-      try { config = require('../../config') }
-      catch(e) {
-        console.log("It appears you've set load_config somehow without having a config file you rascal. Duplicate the default config file and edit or set to false.")
-      }
-    }
-    if(typeof config.period === "undefined") {
-      let cache_period = config.period
-      config.period = period.last_closed()
-      console.log(colors.italic.red(`It appears didn't set a period at all. Period has been set to ${$config.period}`))
-    }
-    else if(config.period > period.last_closed()) {
-      let cache_period = config.period
-      config.period = period.last_closed()
-      console.log(colors.italic.red(`It appears you've set your period to ${cache_period}, has not completed yet. Period has been reset to ${config.period}`))
-    }
-    else if(config.period > CS_MAX_PERIOD_INDEX) {
-      let cache_period = config.period
-      config.period = CS_MAX_PERIOD_INDEX
-      console.log(colors.italic.red(`It appears you've set your period to ${cache_period}, which doesn't exist. Period has been reset to ${config.period}`))
-    }
-    config = Object.assign( require('../../config.default'), config )
-    next(null, config)
-  }
-
-  const override_config_with_params = ( config, next ) => {
-    const optimist = require('optimist')
-          inspect = require('util').inspect
-    config = Object.assign( config, optimist.argv )
-
-    //Remove optimist artifacts
-    delete config.$0
-    delete config._
-
-    next(null, config)
-  }
-
   const run_snapshot = () => {
     let   state = {}
+
+          //Some meta for later.
           state.timestamp_started = (Date.now() / 1000 | 0)
-
-
 
     waterfall([
       //Start waterfall with default state
@@ -81,7 +32,7 @@ module.exports = (COMPLETE) => {
           next(null, state)
         }
       },
-      //Dynamically set globals
+      //Async globals
       require('./tasks/misc/preload'),
       //Set the period map
       require('./tasks/sync/periods'),
@@ -95,6 +46,7 @@ module.exports = (COMPLETE) => {
       require('./tasks/sync/public_keys'),
       //Sync events from the crowdsale contract
       require('./tasks/sync/contract'),
+
       (state, next) => {
         if(config.only_produce_final_snapshot && !state.frozen) {
           console.log("only_produce_final_snapshot is set to true, skipping wallet calculations and snapshot export.")
@@ -172,22 +124,77 @@ module.exports = (COMPLETE) => {
     }
   }
 
-  const configuration_complete = (error, config, callback) => {
-    let table = new Table('Settings')
-    Object.keys(config).forEach((key,index) => {
-      table.addRow([key, config[key]])
-    })
-
-    //Save config globally
-    global.config = config;
-
-    console.log(colors.bold.white(table.setAlign(0, Table.RIGHT).setAlign(1, Table.LEFT).render()))
-    console.log(colors.white('Starting in 5 seconds.'))
-
-    setTimeout( callback, 5000)
-  }
-
   const configure = (callback) => {
+
+    //Show prompt?
+    const show_prompt = next => {
+      prompt = require('./prompt')
+      prompt.start()
+      prompt.get( prompt.schema, (error, config) => {
+        if(error)
+          throw new Error(error)
+        else
+          next(null, config)
+      })
+    }
+
+    //Load and validate config.
+    const load_validate_config = (config, next) => {
+      if(config.load_config) {
+        try { config = require('../../config') }
+        catch(e) {
+          console.log("It appears you've set load_config somehow without having a config file you rascal. Duplicate the default config file and edit or set to false.")
+        }
+      }
+      if(typeof config.period === "undefined") {
+        let cache_period = config.period
+        config.period = period.last_closed()
+        console.log(colors.italic.red(`It appears didn't set a period at all. Period has been set to ${$config.period}`))
+      }
+      else if(config.period > period.last_closed()) {
+        let cache_period = config.period
+        config.period = period.last_closed()
+        console.log(colors.italic.red(`It appears you've set your period to ${cache_period}, has not completed yet. Period has been reset to ${config.period}`))
+      }
+      else if(config.period > CS_MAX_PERIOD_INDEX) {
+        let cache_period = config.period
+        config.period = CS_MAX_PERIOD_INDEX
+        console.log(colors.italic.red(`It appears you've set your period to ${cache_period}, which doesn't exist. Period has been reset to ${config.period}`))
+      }
+      config = Object.assign( require('../../config.default'), config )
+      next(null, config)
+    }
+
+    const override_config_with_params = ( config, next ) => {
+      const optimist = require('optimist')
+            inspect = require('util').inspect
+      config = Object.assign( config, optimist.argv )
+
+      //Remove optimist artifacts
+      delete config.$0
+      delete config._
+
+      next(null, config)
+    }
+
+    const configuration_complete = (error, config, callback) => {
+      let table = new Table('Settings')
+
+      //Display session parameters
+      Object.keys(config).forEach((key,index) => {
+        table.addRow([key, config[key]])
+      })
+
+      //Save config globally
+      global.config = config;
+
+      //Some screen output.
+      console.log(colors.bold.white(table.setAlign(0, Table.RIGHT).setAlign(1, Table.LEFT).render()))
+      console.log(colors.white('Starting in 5 seconds.'))
+
+      setTimeout( callback, 5000 )
+    }
+
     waterfall(
       [ show_prompt, load_validate_config, override_config_with_params ],
       (error, config) => configuration_complete(error, config, callback)
