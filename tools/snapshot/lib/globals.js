7d3d3259f39c06d6b24c343770545bbdc8987d5a
Sandwich <hi.na3s@gmail.com>
2018-05-22 03:54:41 +0200

    bump version

--- tools/snapshot/lib/globals.js
+++ tools/snapshot/lib/globals.js
@@ -1,6 +1,6 @@
 const bn = require('bignumber.js')
 
-global.VERSION                            = "0.4.0-beta.10"
+global.VERSION                            = "0.4.0-stable"
 
 //Crowdsale Globals
 global.CS_ADDRESS_CROWDSALE               = "0xd0a6e6c54dbc68db5db3a091b171a77407ff7ccf"
